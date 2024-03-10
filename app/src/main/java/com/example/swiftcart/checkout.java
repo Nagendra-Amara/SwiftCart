7624b60b9ea805c91c7019c5b6abce4045f8d83c
Nagendra-Amara <amara.nagendhra@gmail.com>
2024-03-10 12:41:45 +0530

    Multiple items recognisation

--- app/src/main/java/com/example/swiftcart/checkout.java
+++ app/src/main/java/com/example/swiftcart/checkout.java
@@ -3,18 +3,26 @@ package com.example.swiftcart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -29,24 +37,22 @@ public class checkout extends AppCompatActivity {
         setContentView(R.layout.activity_checkout);
         ImageView qr = findViewById(R.id.qr);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         if(getSupportActionBar() != null)getSupportActionBar().hide();
 
 
-        //getting text from input text field.
+        
         String myText = cart.getTotal();
         String cartId = dashboard.getCartId();
 
         DatabaseReference dbref= FirebaseDatabase.getInstance().getReference().child("customers");
-        // we are use add listerner
-        // for event listener method
-        // which is called with query.
-        String time;
+        
         Query query=dbref.child(cartId);
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // remove the value at reference
+                
                 dataSnapshot.getRef().removeValue();
             }
 
@@ -55,20 +61,58 @@ public class checkout extends AppCompatActivity {
 
             }
         });
-        //initializing MultiFormatWriter for QR code
+
+        db.collection(cartId).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            WriteBatch batch = db.batch();
+                            QuerySnapshot querySnapshot = task.getResult();
+                            for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                                batch.delete(document.getReference());
+                            }
+                            batch.commit()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+
+                                            } else {
+
+                                            }
+                                        }
+                                    });
+                        } else {
+
+                        }
+                    }
+                });
+
+
+
+
+
         MultiFormatWriter mWriter = new MultiFormatWriter();
         try {
-            //BitMatrix class to encode entered text and set Width & Height
+            
             BitMatrix mMatrix = mWriter.encode(myText, BarcodeFormat.QR_CODE, 400,400);
             BarcodeEncoder mEncoder = new BarcodeEncoder();
             Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
             qr.setImageBitmap(mBitmap);//Setting generated QR code to imageView
-            // to hide the keyboard
-            //InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            
 
         } catch (WriterException e) {
             e.printStackTrace();
         }
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(this,dashboard.class);
+        startActivity(i);
     }
 }
